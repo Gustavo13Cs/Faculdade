6e626178e70da6333b49b26e1bb18e4d0904e210
Gustavo Cunha Santos <105940671+Gustavo13Cs@users.noreply.github.com>
2024-03-20 23:50:42 -0300

    .

---
+++ Linguagem orientadas a objetos/numeroPar.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class numeroPar {
+    public static void main(String [] args) {
+
+        Scanner valor = new Scanner(System.in);
+
+        System.out.println("informe um numero inteiro: ");
+
+        int numero = valor.nextInt();
+
+        if(numero % 2 == 0) {
+            System.out.println("O numero digitado é par");
+        }
+        else {
+            System.out.println("O numero digitado não é par");
+        }
+        System.exit(0);
+    }
+}
